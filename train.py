34565abca4e088382f9377bb3e4a9279cdb6d968
abdussamettrkr <abdussamettrkr@gmail.com>
2022-06-19 22:17:45 +0300

    Modeles and losses are initialized for DINO

--- train.py
+++ train.py
@@ -1,81 +1,27 @@
 import argparse
 import os
+
+import torch
+import torch.nn as nn
+from torchvision import datasets,transforms
+from torchvision import models as torchvision_models
+
+import utils 
+from dataset.augmentation import *
+import models.vit as vits
+from models.vit import DINOHead
+from losses.loss import DINOLoss
+
 from pyexpat import model
 
 
-models = ['vit_tiny','vit_small','vit_base','xcit','deit_tiny','deit_small']
 
-parser = argparse.ArgumentParser('DINO', add_help=False)
-parser.add_argument('--model',default ='vit_small',type=str,choices= models,help="Name of the model. Default vit_small")
-parser.add_argument('--patch_size',default=16,type=int,help="VIT square patch sizes. Default 16")
-parser.add_argument('--out_dim',default=65536,type=int,help="Dimensionality of the DINO head output, bigger valus for complex tax. Default 65536")
-parser.add_argument('--norm_last_layer',default=True,type=bool,help="Whether or not normilize the last layer of DINO head. Increases Training stability. Default True")
-parser.add_argument('--momentum_teacher',default=0.996,type=float,help="Base EMA paramater used for teacher parameter uptade. The value increased to 1 during training with Cosine Schedule. Bigger value for smaller batch-size. Default 0.996")
-parser.add_argument('--use_bn_in_head',default=False,type=bool,help="Wheter or not use batch normilization in projection head. Default False")
+
+
 
 
 # Temperature teacher parameters
-parser.add_argument('--warmup_teacher_temp', default=0.04, type=float,
-    help="""Initial value for the teacher temperature: 0.04 works well in most cases.
-    Try decreasing it if the training loss does not decrease.""")
-parser.add_argument('--teacher_temp', default=0.04, type=float, help="""Final value (after linear warmup)
-    of the teacher temperature. For most experiments, anything above 0.07 is unstable. We recommend
-    starting with the default value of 0.04 and increase this slightly if needed.""")
-parser.add_argument('--warmup_teacher_temp_epochs', default=0, type=int,
-    help='Number of warmup epochs for the teacher temperature (Default: 30).')
 
-# Training/Optimization parameters
-parser.add_argument('--use_fp16', type=utils.bool_flag, default=True, help="""Whether or not
-    to use half precision for training. Improves training time and memory requirements,
-    but can provoke instability and slight decay of performance. We recommend disabling
-    mixed precision if the loss is unstable, if reducing the patch size or if training with bigger ViTs.""")
-parser.add_argument('--weight_decay', type=float, default=0.04, help="""Initial value of the
-    weight decay. With ViT, a smaller value at the beginning of training works well.""")
-parser.add_argument('--weight_decay_end', type=float, default=0.4, help="""Final value of the
-    weight decay. We use a cosine schedule for WD and using a larger decay by
-    the end of training improves performance for ViTs.""")
-parser.add_argument('--clip_grad', type=float, default=3.0, help="""Maximal parameter
-    gradient norm if using gradient clipping. Clipping with norm .3 ~ 1.0 can
-    help optimization for larger ViT architectures. 0 for disabling.""")
-parser.add_argument('--batch_size_per_gpu', default=64, type=int,
-    help='Per-GPU batch-size : number of distinct images loaded on one GPU.')
-parser.add_argument('--epochs', default=100, type=int, help='Number of epochs of training.')
-parser.add_argument('--freeze_last_layer', default=1, type=int, help="""Number of epochs
-    during which we keep the output layer fixed. Typically doing so during
-    the first epoch helps training. Try increasing this value if the loss does not decrease.""")
-parser.add_argument("--lr", default=0.0005, type=float, help="""Learning rate at the end of
-    linear warmup (highest LR used during training). The learning rate is linearly scaled
-    with the batch size, and specified here for a reference batch size of 256.""")
-parser.add_argument("--warmup_epochs", default=10, type=int,
-    help="Number of epochs for the linear learning-rate warm up.")
-parser.add_argument('--min_lr', type=float, default=1e-6, help="""Target LR at the
-    end of optimization. We use a cosine LR schedule with linear warmup.""")
-parser.add_argument('--optimizer', default='adamw', type=str,
-    choices=['adamw', 'sgd', 'lars'], help="""Type of optimizer. We recommend using adamw with ViTs.""")
-parser.add_argument('--drop_path_rate', type=float, default=0.1, help="stochastic depth rate")
-
-# Multi-crop parameters
-parser.add_argument('--global_crops_scale', type=float, nargs='+', default=(0.4, 1.),
-    help="""Scale range of the cropped image before resizing, relatively to the origin image.
-    Used for large global view cropping. When disabling multi-crop (--local_crops_number 0), we
-    recommand using a wider range of scale ("--global_crops_scale 0.14 1." for example)""")
-parser.add_argument('--local_crops_number', type=int, default=8, help="""Number of small
-    local views to generate. Set this parameter to 0 to disable multi-crop training.
-    When disabling multi-crop we recommend to use "--global_crops_scale 0.14 1." """)
-parser.add_argument('--local_crops_scale', type=float, nargs='+', default=(0.05, 0.4),
-    help="""Scale range of the cropped image before resizing, relatively to the origin image.
-    Used for small local view cropping of multi-crop.""")
-
-# Misc
-parser.add_argument('--data_path', default='/path/to/imagenet/train/', type=str,
-    help='Please specify path to the ImageNet training data.')
-parser.add_argument('--output_dir', default=".", type=str, help='Path to save logs and checkpoints.')
-parser.add_argument('--saveckp_freq', default=20, type=int, help='Save checkpoint every x epochs.')
-parser.add_argument('--seed', default=0, type=int, help='Random seed.')
-parser.add_argument('--num_workers', default=10, type=int, help='Number of data loading workers per GPU.')
-parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up
-    distributed training; see https://pytorch.org/docs/stable/distributed.html""")
-parser.add_argument("--local_rank", default=0, type=int, help="Please ignore and do not set this argument.")
 
 
 
@@ -141,4 +87,209 @@ def train_one_epoch(student, teacher, teacher_without_ddp, dino_loss, data_loade
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+
+def parse_opt():
+    models = ['vit_tiny','vit_small','vit_base','xcit','deit_tiny','deit_small']
+
+    parser = argparse.ArgumentParser('DINO', add_help=False)
+    parser.add_argument('--model',default ='vit_small',type=str,choices= models,help="Name of the model. Default vit_small")
+    parser.add_argument('--patch_size',default=16,type=int,help="VIT square patch sizes. Default 16")
+    parser.add_argument('--out_dim',default=65536,type=int,help="Dimensionality of the DINO head output, bigger valus for complex tax. Default 65536")
+    parser.add_argument('--norm_last_layer',default=True,type=bool,help="Whether or not normilize the last layer of DINO head. Increases Training stability. Default True")
+    parser.add_argument('--momentum_teacher',default=0.996,type=float,help="Base EMA paramater used for teacher parameter uptade. The value increased to 1 during training with Cosine Schedule. Bigger value for smaller batch-size. Default 0.996")
+    parser.add_argument('--use_bn_in_head',default=False,type=bool,help="Wheter or not use batch normilization in projection head. Default False")
+
+    #0.04 works well in most cases. Try decreasing it if the training loss does not decrease.
+    parser.add_argument('--warmup_teacher_temp', default=0.04, type=float, help="""Initial value for the teacher temperature""")
+
+    #For most experiments, anything above 0.07 is unstable. We recommend starting with the default value of 0.04 and increase this slightly if needed.
+    parser.add_argument('--teacher_temp', default=0.04, type=float, help="""Final value (after linear warmup)of the teacher temperature. """)
+
+    parser.add_argument('--warmup_teacher_temp_epochs', default=30, type=int, help='Number of warmup epochs for the teacher temperature (Default: 30).')
+
+    # Training/Optimization parameters
+    # Improves training time and memory requirements, but can provoke instability and slight decay of performance. We recommend disabling mixed precision if the loss is unstable, if reducing the patch size or if training with bigger ViTs.
+    parser.add_argument('--use_fp16', type=utils.bool_flag, default=True, help="""Whether or notto use half precision for training.""")
+
+    # With ViT, a smaller value at the beginning of training works well.
+    parser.add_argument('--weight_decay', type=float, default=0.04, help="""Initial value of the weight decay.""")
+
+    # We use a cosine schedule for WD and using a larger decay by the end of training improves performance for ViTs.
+    parser.add_argument('--weight_decay_end', type=float, default=0.4, help="""Final value of the weight decay.""")
+    
+    # Clipping with norm .3 ~ 1.0 canhelp optimization for larger ViT architectures. 0 for disabling.
+    parser.add_argument('--clip_grad', type=float, default=3.0, help="""Maximal parameter gradient norm if using gradient clipping.""")
+    parser.add_argument('--batch_size_per_gpu', default=64, type=int, help='Per-GPU batch-size : number of distinct images loaded on one GPU.')
+    parser.add_argument('--epochs', default=100, type=int, help='Number of epochs of training.')
+
+    # Typically doing so during the first epoch helps training. Try increasing this value if the loss does not decrease.
+    parser.add_argument('--freeze_last_layer', default=1, type=int, help="""Number of epochs during which we keep the output layer fixed.""")
+
+    # The learning rate is linearly scaled with the batch size, and specified here for a reference batch size of 256.
+    parser.add_argument("--lr", default=0.0005, type=float, help="""Learning rate at the end of linear warmup (highest LR used during training).""")
+    
+    parser.add_argument("--warmup_epochs", default=10, type=int, help="Number of epochs for the linear learning-rate warm up.")
+    parser.add_argument('--min_lr', type=float, default=1e-6, help="""Target LR at the end of optimization. We use a cosine LR schedule with linear warmup.""")
+    parser.add_argument('--optimizer', default='adamw', type=str, choices=['adamw', 'sgd', 'lars'], help="""Type of optimizer. We recommend using adamw with ViTs.""")
+
+    parser.add_argument('--drop_path_rate', type=float, default=0.1, help="stochastic depth rate")
+
+    # Multi-crop parameters
+    # When disabling multi-crop (--local_crops_number 0), we recommand using a wider range of scale ("--global_crops_scale 0.14 1." for example)
+    parser.add_argument('--global_crops_scale', type=float, nargs='+', default=(0.4, 1.), help="""Scale range of the cropped image before resizing, relatively to the origin image. Used for large global view cropping.""")
+
+    #When disabling multi-crop we recommend to use "--global_crops_scale 0.14 1." 
+    parser.add_argument('--local_crops_number', type=int, default=8, help="""Number of small local views to generate. Set this parameter to 0 to disable multi-crop training. """)
+    parser.add_argument('--local_crops_scale', type=float, nargs='+', default=(0.05, 0.4), help="""Scale range of the cropped image before resizing, relatively to the origin image. Used for small local view cropping of multi-crop.""")
+
+    # Misc
+    parser.add_argument('--data_path', default='/path/to/imagenet/train/', type=str, help='Please specify path to the ImageNet training data.')
+    parser.add_argument('--output_dir', default=".", type=str, help='Path to save logs and checkpoints.')
+    parser.add_argument('--saveckp_freq', default=20, type=int, help='Save checkpoint every x epochs.')
+    parser.add_argument('--seed', default=41, type=int, help='Random seed.')
+    parser.add_argument('--num_workers', default=10, type=int, help='Number of data loading workers per GPU.')
+    parser.add_argument("--dist_url", default="env://", type=str, help="""url used to set up distributed training; see https://pytorch.org/docs/stable/distributed.html""")
+    parser.add_argument("--local_rank", default=0, type=int, help="Please ignore and do not set this argument.")
+
+    return parser
+
+
+def main(args):
+    utils.set_random_seeds(args.seed)
+
+    transform = DataAugmentationDINO(
+        global_crops_scale=args.global_crops_scale,
+        local_crops_number=args.local_crops_number,
+        local_crops_scale=args.local_crops_scale)
+    
+    dataset = datasets(args.data_path, transform=transform)
+    sampler = torch.utils.data.DistributedSampler(dataset, shuffle=True)    
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        sampler=sampler,
+        batch_size=args.batch_size_per_gpu,
+        num_workes=args.num_works,
+        pin_memory=True,
+        drop_last=True
+    )
+    print(f"Data loaded: there are {len(dataset)} images.")
+
+    student,teacher = get_models(args)
+
+
+    # ============ preparing loss ... ============
+    dino_loss = DINOLoss(
+        args.out_dim,
+        args.local_crops_number + 2,  # total number of crops = 2 global crops + local_crops_number
+        args.warmup_teacher_temp,
+        args.teacher_temp,
+        args.warmup_teacher_temp_epochs,
+        args.epochs,
+    ).cuda()
+
+    # ============ preparing optimizer ... ============
+    params_groups = utils.get_params_groups(student)
+    if args.optimizer == "adamw":
+        optimizer = torch.optim.AdamW(params_groups)  # to use with ViTs
+    elif args.optimizer == "sgd":
+        optimizer = torch.optim.SGD(params_groups, lr=0, momentum=0.9)  # lr is set by scheduler
+    elif args.optimizer == "lars":
+        optimizer = utils.LARS(params_groups)  # to use with convnet and large batches
+    # for mixed precision training
+    fp16_scaler = None
+    if args.use_fp16:
+        fp16_scaler = torch.cuda.amp.GradScaler()
+
+    # ============ init schedulers ... ============
+    lr_schedule = utils.cosine_scheduler(
+        args.lr * (args.batch_size_per_gpu * utils.get_world_size()) / 256.,  # linear scaling rule
+        args.min_lr,
+        args.epochs, len(data_loader),
+        warmup_epochs=args.warmup_epochs,
+    )
+    wd_schedule = utils.cosine_scheduler(
+        args.weight_decay,
+        args.weight_decay_end,
+        args.epochs, len(data_loader),
+    )
+    # momentum parameter is increased to 1. during training with a cosine schedule
+    momentum_schedule = utils.cosine_scheduler(args.momentum_teacher, 1,
+                                               args.epochs, len(data_loader))
+    print(f"Loss, optimizer and schedulers ready.")
+
+
+    ##Optionally resume training
+
+
+def get_models(args):
+
+    if args.arch in vits.__dict__.keys():
+        student = vits.__dict__[args.arch](
+            patch_size=args.patch_size,
+            drop_path_rate=args.drop_path_rate,  # stochastic depth
+        )
+        teacher = vits.__dict__[args.arch](patch_size=args.patch_size)
+        embed_dim = student.embed_dim
+    # if the network is a XCiT
+    elif args.arch in torch.hub.list("facebookresearch/xcit:main"):
+        student = torch.hub.load('facebookresearch/xcit:main', args.arch,
+                                 pretrained=False, drop_path_rate=args.drop_path_rate)
+        teacher = torch.hub.load('facebookresearch/xcit:main', args.arch, pretrained=False)
+        embed_dim = student.embed_dim
+    # otherwise, we check if the architecture is in torchvision models
+    elif args.arch in torchvision_models.__dict__.keys():
+        student = torchvision_models.__dict__[args.arch]()
+        teacher = torchvision_models.__dict__[args.arch]()
+        embed_dim = student.fc.weight.shape[1]
+    else:
+        print(f"Unknow architecture: {args.arch}")
+
+
+    # multi-crop wrapper handles forward with inputs of different resolutions
+    student = utils.MultiCropWrapper(student, DINOHead(
+        embed_dim,
+        args.out_dim,
+        use_bn=args.use_bn_in_head,
+        norm_last_layer=args.norm_last_layer,
+    ))
+
+    teacher = utils.MultiCropWrapper(teacher, DINOHead(
+        embed_dim, 
+        args.out_dim, 
+        args.use_bn_in_head),
+    )
+
+    student, teacher = student.cuda(), teacher.cuda()
+
+
+    # synchronize batch norms (if any)
+    if utils.has_batchnorms(student):
+        student = nn.SyncBatchNorm.convert_sync_batchnorm(student)
+        teacher = nn.SyncBatchNorm.convert_sync_batchnorm(teacher)
+
+        # we need DDP wrapper to have synchro batch norms working...
+        teacher = nn.parallel.DistributedDataParallel(teacher, device_ids=[args.gpu])
+        teacher_without_ddp = teacher.module
+    else:
+        # teacher_without_ddp and teacher are the same thing
+        teacher_without_ddp = teacher
+
+    student = nn.parallel.DistributedDataParallel(student, device_ids=[args.gpu])
+    # teacher and student start with the same weights
+    teacher_without_ddp.load_state_dict(student.module.state_dict())
+    # there is no backpropagation through the teacher, so no need for gradients
+    for p in teacher.parameters():
+        p.requires_grad = False
+    print(f"Student and Teacher are built: they are both {args.arch} network.")
+
+    return student,teacher
+
+
+
+if __name__ == "__name__":
+    opt = parse_opt()
+    
+    #main(opt)
 
